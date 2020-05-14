432fbf1756ae0e174488cfcf1191515ea4f0f158
Luc Claustres <luc.claustres@orange.fr>
2020-05-14 08:11:25 +0200

    Updated data

--- generate-data-jobfile.js
+++ generate-data-jobfile.js
@@ -5,7 +5,9 @@ const program = require('commander')
 program
     .option('-s, --start [date]', 'Change the start date of the data to be generated (defaults to 2020-03-01)', moment('2020-03-01').format('YYYY-MM-DD'))
     .option('-e, --end [date]', 'Change the end date of the data to be generated (defaults to yesterday)', moment().subtract(1, 'day').format('YYYY-MM-DD'))
+    .option('-po, --port [port]', 'Change the port to be used (defaults to 5000)', 5000)
     .parse(process.argv)
+const port = (program.port ? program.port + 1 : 5001)
 
 let start = moment(program.start)
 if (!start.isValid()) {
@@ -39,42 +41,42 @@ module.exports = {
         csse: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler csse-jobfile.js --port 3031 --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler csse-jobfile.js --port ${port} --date <%= date.format('YYYY-MM-DD') %>`
         },
         spfHospitalieres: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler spf-donnees-hospitalieres-jobfile.js --port 3031 --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler spf-donnees-hospitalieres-jobfile.js --port ${port} --date <%= date.format('YYYY-MM-DD') %>`
         },
         spfUrgences: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler spf-donnees-urgences-sos-medecins-jobfile.js --port 3031 --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler spf-donnees-urgences-sos-medecins-jobfile.js --port ${port} --date <%= date.format('YYYY-MM-DD') %>`
         },
         spfLaboratoires: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler spf-donnees-laboratoires-jobfile.js --port 3031 --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler spf-donnees-laboratoires-jobfile.js --port ${port} --date <%= date.format('YYYY-MM-DD') %>`
         },
         regionsPoint: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler france-regions-jobfile.js --port 3031 --geometry Point --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler france-regions-jobfile.js --port ${port} --geometry Point --date <%= date.format('YYYY-MM-DD') %>`
         },
         regionsPolygon: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler france-regions-jobfile.js --port 3031 --geometry Polygon --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler france-regions-jobfile.js --port ${port} --geometry Polygon --date <%= date.format('YYYY-MM-DD') %>`
         },
         departementsPoint: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler france-departements-jobfile.js --port 3031 --geometry Point --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler france-departements-jobfile.js --port ${port} --geometry Point --date <%= date.format('YYYY-MM-DD') %>`
         },
         departementsPolygon: {
           hook: 'runCommand',
           stdout: true, stderr: true,
-          command: `krawler france-departements-jobfile.js --port 3031 --geometry Polygon --date <%= date.format('YYYY-MM-DD') %>`
+          command: `krawler france-departements-jobfile.js --port ${port} --geometry Polygon --date <%= date.format('YYYY-MM-DD') %>`
         }
       }
     },
